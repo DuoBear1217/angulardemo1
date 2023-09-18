da707027741e3212aa6027891e7d8792dfd3bb48
DuoBear1217 <duoplay91095@gmail.com>
2023-09-18 16:45:27 +0800

    '建立註冊以及登入的表單，並照網站設定驗證'

--- BuyWeb/src/app/login/login/login.component.ts
+++ BuyWeb/src/app/login/login/login.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -12,7 +12,41 @@ export class LoginComponent {
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginform = this.formBuilder.group({
+      account: [
+        '',
+        [
+          Validators.required,
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16)
+        ]
+      ]
+    });
+  }
 
+  getErrorMessage(formControl: FormControl): string {
+    let errorMessage: string='';
+    if (!formControl.errors || formControl.pristine) {
+      errorMessage = '';
+    } else if (formControl.errors['required']) {
+      errorMessage = '此欄位必填';
+    } else if (formControl.errors['pattern']) {
+      errorMessage = '格式有誤，請重新輸入';
+    } else if (formControl.errors['minlength']) {
+      errorMessage = '密碼長度最短不得低於8碼';
+    } else if (formControl.errors['maxlength']) {
+      errorMessage = '密碼長度最長不得超過16碼';
+    }
+    return errorMessage;
+  }
 
+  login() {
+    console.log(this.loginform.value);
   }
 }
--- BuyWeb/src/app/login/register/register.component.ts
+++ BuyWeb/src/app/login/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  register!: FormGroup;
 
+  constructor(private formBuild: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.register = this.formBuild.group({
+      account: [
+        'OAO', //預設值
+      ]
+    })
+
+  }
 }
